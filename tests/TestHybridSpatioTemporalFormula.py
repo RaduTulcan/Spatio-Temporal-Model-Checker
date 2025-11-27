44d33ed7e0f4298e2adf6ee0707a106a20b27837
Kevin Zhou <khzhou96@gmail.com>
2025-11-27 12:08:22 +0900

    add some tests for temporal and hybridspatiotemporal formulas

--- tests/TestHybridSpatioTemporalFormula.py
+++ tests/TestHybridSpatioTemporalFormula.py
@@ -1,9 +1,45 @@
 import unittest
 
+from formula_types.ClassicalLogicFormula import Prop, Not, And, If, Iff
+from formula_types.SpatialFormula import Left, Right, Front, Back
+from formula_types.TemporalFormula import Eventually, Always, Until
+from formula_types.HybridFormula import Nom, At, Bind
+
 
 class TestHybridSpatioTemporalFormula(unittest.TestCase):
+    def setUp(self):
+        self.point1 = (0,0)
+        self.point2 = (0,1)
+        self.point3 = (1,0)
+        self.point4 = (1,1)
+
+        self.grid_size = (2,2)
+
+        self.trace = [
+            {
+                'a' : [self.point1],
+                'b' : [],
+                'c' : [self.point1],
+                'd' : [],
+            }
+        ] * 9 + [
+            {
+                'a' : [],
+                'b' : [self.point1],
+                'c' : [self.point1],
+                'd' : [],
+            }
+        ]
+
     def test_validities_evaluet(self):
-        pass
+        for p1 in ['a', 'b', 'c', 'd']:
+            for p2 in ['a', 'b', 'c', 'd']:
+                prop1 = Prop(p1)
+                prop2 = Prop(p2)
+                left_until = Left("LEFT", Until("UNTIL", prop1, prop2))
+                until_left = Until("UNTIL", Left("LEFT", prop1), Left("LEFT", prop2))
+                for pt in [self.point1, self.point2, self.point3, self.point4]:
+                    self.assertTrue(Iff("IFF", left_until, until_left).evaluate(self.trace, pt, self.grid_size))
 
     def test_complex_formula1_evaluate(self):
         pass
--- tests/TestTemporalFormula.py
+++ tests/TestTemporalFormula.py
@@ -1,18 +1,53 @@
 import unittest
 
+from formula_types.ClassicalLogicFormula import Prop
+from formula_types.TemporalFormula import Next, Always, Eventually, Until
 
 class TestTemporalFormula(unittest.TestCase):
+    def setUp(self):
+        self.point1 = (0,0)
+        self.point2 = (0,1)
+        self.point3 = (1,0)
+        self.point4 = (1,1)
+
+        self.grid_size = (2,2)
+
+        self.trace = [
+            {
+                'a' : [self.point1],
+                'b' : [],
+                'c' : [self.point1],
+                'd' : [],
+            }
+        ] * 9 + [
+            {
+                'a' : [],
+                'b' : [self.point1],
+                'c' : [self.point1],
+                'd' : [],
+            }
+        ]
+
     def test_next_evaluate(self):
         pass
 
     def test_globally_evaluate(self):
-        pass
+        always_a = Always("ALWAYS", Prop("a"))
+        self.assertFalse(always_a.evaluate(self.trace, self.point1, self.grid_size))
+        self.assertTrue(always_a.evaluate(self.trace[:5], self.point1, self.grid_size))
 
     def test_eventually_evaluate(self):
-        pass
+        eventually_b = Eventually("EVENTUALLY", Prop("b"))
+        self.assertTrue(eventually_b.evaluate(self.trace, self.point1, self.grid_size))
+        self.assertFalse(eventually_b.evaluate(self.trace[:5], self.point1, self.grid_size))
 
     def test_until_evaluate(self):
-        pass
+        a_until_b = Until("UNTIL", Prop("a"), Prop("b"))
+        c_until_d = Until("UNTIL", Prop("c"), Prop("d"))
+        a_until_b_until_c_until_d = Until("UNTIL", a_until_b, c_until_d)
+        self.assertTrue(a_until_b.evaluate(self.trace, self.point1, self.grid_size))
+        self.assertFalse(c_until_d.evaluate(self.trace, self.point1, self.grid_size))
+        self.assertFalse(a_until_b_until_c_until_d.evaluate(self.trace, self.point1, self.grid_size)) 
 
     def test_complex_formula1_evaluate(self):
         pass
