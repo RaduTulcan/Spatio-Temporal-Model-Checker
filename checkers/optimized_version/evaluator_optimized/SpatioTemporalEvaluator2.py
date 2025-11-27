c3f848152e69d5e702b11a6956ca411c38e2bb62
rosebohrer <rose.bohrer@aist.go.jp>
2025-11-27 17:44:54 +0900

    Rewrite parsing functionality in movement evaluator to support test case formulas; add support for staying in place

--- checkers/optimized_version/evaluator_optimized/SpatioTemporalEvaluator2.py
+++ checkers/optimized_version/evaluator_optimized/SpatioTemporalEvaluator2.py
@@ -3,12 +3,17 @@ from itertools import chain, combinations, product
 
 from formula_types.HybridSpatioTemporalFormula import HybridSpatioTemporalFormula
 from parsers.HybridSpatioTemporalFormulaParser import HybridSpatioTemporalParser, tokenize
+from formula_types.HybridFormula import Nom, At, Bind
+from formula_types.ClassicalLogicFormula import Verum, Falsum, Prop, Not, And, Or
+from formula_types.SpatialFormula import Front, Back, Left, Right
+
 
 DIRECTIONS = {
     "Left": (0, -1),
     "Right": (0, 1),
     "Front": (-1, 0),
     "Back": (1, 0),
+    "Stay": (0, 0),
 }
 
 
@@ -88,65 +93,150 @@ def parse_fixed_offset(formula: str):
     return reference_car, dependent_car, offset
 
 
-def parse_fixed_movement(formula):
-    branches = [b.strip() for b in formula.split("|")]
-    offsets = []
-    first_branch = branches[0]
-    tokens_first = get_tokens(first_branch)
+#If fml is a (nested) disjunction, return the list of disjuncts
+def branches_of(fml):
+    if isinstance(fml, Or):
+        return branches_of(fml.left) + branches_of(fml.right)
+    else:
+        return [fml]
+    
+#If fml is a (nested) disjunction, return the first disjunct.
+def debranch(fml):
+    return branches_of(fml)[0]
+
+# Returns whether fml is an end-checking formula (! X 1)
+def is_end_check(fml):
+    if not isinstance(fml, Not):
+        return False
+    next = fml.operand
+    if next.op != "X":
+        return False
+    return isinstance(next.operand, Verum)
+
+# remove disjunct (! X 1)
+def strip_end_check(fml):
+    if fml.op != "∨":
+        return fml
+    branches = branches_of(fml)
+    clean_branches = [x for x in branches if not is_end_check(x)]
+    if len(clean_branches) != 1:
+        return None
+    return clean_branches[0]
+
+# Unpack the syntactic pattern used in movement specs:  G @ ↓ X @
+def get_movement_pattern(fml):
+    if fml.op != "G":
+        return None
+    at_outer = fml.operand
+    if at_outer.op[0] != "@":
+        return None
+    arrow = at_outer.operand
+    if arrow.op[0] != "↓":
+        return None
+    next = strip_end_check(arrow.operand)
+    if next == None or next.op != "X":
+        return None
+    at_inner = next.operand
+    if at_inner.op[0] != "@":
+        return None
+    if at_outer.op != at_inner.op:
+        return None
+    vehicle = at_outer.name
+    tmp_var = arrow.name
+    inner_branches = branches_of(at_inner.operand)
+    return (vehicle, tmp_var, inner_branches)
+
+def is_left_of(fml, tmp_var): return isinstance(fml, Left) and fml.operand.name == tmp_var
+def is_right_of(fml, tmp_var): return isinstance(fml, Right) and fml.operand.name == tmp_var
+def is_front_of(fml, tmp_var): return isinstance(fml, Front) and fml.operand.name == tmp_var
+def is_back_of(fml, tmp_var): return isinstance(fml, Back) and fml.operand.name == tmp_var
+def is_stay_of(fml, tmp_var): return isinstance(fml, Nom) and fml.name == tmp_var
+
+def branches_to_directions(inner_branches, tmp_var):
+    lefts = [x for x in inner_branches if is_left_of(x, tmp_var)]
+    rights = [x for x in inner_branches if is_right_of(x, tmp_var)]
+    fronts = [x for x in inner_branches if is_front_of(x, tmp_var)]
+    backs = [x for x in inner_branches if is_back_of(x, tmp_var)]
+    stays = [x for x in inner_branches if is_stay_of(x, tmp_var)]
+    stops = [x for x in inner_branches if is_end_check(x)]
+    extras = [x for x in inner_branches if not x in (lefts + rights + fronts + backs + stays + stops)]
+    if len(extras) > 0:
+        return None
+    directions = []
+    if (len(lefts) > 0): directions = directions + ["Left"]
+    if (len(rights) > 0): directions = directions + ["Right"]
+    if (len(fronts) > 0): directions = directions + ["Front"]
+    if (len(backs) > 0): directions = directions + ["Back"]
+    if (len(stays) > 0): directions = directions + ["Stay"]
+    return directions
+    
+
+def parse_fixed_movement(fml_str, formula):
+    
+    #branches = [b.strip() for b in formula.split("|")]
+    #first_branch = branches[0]
+    #tokens_first = get_tokens(first_branch)
 
     # formula must start with globally operator
-    if tokens_first[0] != "G":
-        return None, None
+    #if tokens_first[0] != "G":
+    #    return None, None
 
     # followed by at-operator and a nominal
-    if not re.fullmatch(r"@z[0-9_]*", tokens_first[1]):
-        return None, None
+    #if not re.fullmatch(r"@z[0-9_]*", tokens_first[1]):
+    #    return None, None
 
     # followed by a binding operator
-    if not re.fullmatch(r"↓z[0-9_]*", tokens_first[2]):
-        return None, None
+    #if not re.fullmatch(r"↓z[0-9_]*", tokens_first[2]):
+    #    return None, None
 
     # followed by next operator
-    if tokens_first[3] != "X":
-        return None, None
+    #if tokens_first[3] != "X":
+    #    return None, None
 
     # the nominals for at-operator must match
-    if tokens_first[1] != tokens_first[4]:
-        return None, None
+    #if tokens_first[1] != tokens_first[4]:
+    #    return None, None
 
     # "free" nominals used after the binding operator must match the nominial of the operator
-    if tokens_first[-1] != tokens_first[2][1:]:
+    #if tokens_first[-1] != tokens_first[2][1:]:
+     #   return None, None
+    pat = get_movement_pattern(debranch(formula))
+    if pat == None:
         return None, None
-
-    car = tokens_first[1][1:]
-    directions = tokens_first[5:-1]
+    (car, tmp_var, inner_branches) = pat
+    directions = branches_to_directions(inner_branches, tmp_var)
+    if directions == None:
+        return None, None
+    #car   = tokens_first[1][1:]
+    #directions = tokens_first[5:-1]
 
     # check that all other tokens are only directions
-    if len(directions) == 0:
-        return None, None
+    #if len(directions) == 0:
+    #    return None, None
 
     for d in directions:
         if d != "Left" and d != "Right" and d != "Back" and d != "Front":
             return None, None
 
+    offsets=[]
     offsets.append(dirs_to_offset(directions))
-
-    for b in branches[1:]:
-        tok = get_tokens(b)
-        directions = tok[:-1]
-
-        # "free" nominals used after the binding operator must match the nominial of the operator
-        if tok[-1] != tokens_first[2][1:]:
-            return None, None
-
-        for d in directions:
-            if d != "Left" and d != "Right" and d != "Back" and d != "Front":
-                return None, None
-
-        offsets.append(dirs_to_offset(directions))
-
     return car, offsets
 
+    #for b in branches[1:]:
+        #tok = get_tokens(b)
+        #directions = tok[:-1]
+
+        # "free" nominals used after the binding operator must match the nominial of the operator
+        #if tok[-1] != tokens_first[2][1:]:
+            #return None, None
+
+#        for d in directions:
+            #if d != "Left" and d != "Right" and d != "Back" and d != "Front":
+                #return None, None
+
+#        offsets.append(dirs_to_offset(directions))
+
+ 
 
 def divide_cars_in_types(assumptions, nominals):
     static_cars = []
@@ -156,6 +246,7 @@ def divide_cars_in_types(assumptions, nominals):
     fixed_movement_car_names = []
 
     for a in assumptions:
+        a_fml = HybridSpatioTemporalParser(tokenize(a)).parse()
         # car is static
         static_car = parse_static_car(a)
         if static_car:
@@ -172,7 +263,7 @@ def divide_cars_in_types(assumptions, nominals):
             dependent_cars[reference_car].append((dependent_car, dependent_offset))
 
         # car has a fixed movement
-        fixed_movement_car, self_offsets = parse_fixed_movement(a)
+        fixed_movement_car, self_offsets = parse_fixed_movement(a, a_fml)
 
         if fixed_movement_car and self_offsets:
             fixed_movement_car_names.append(fixed_movement_car)
@@ -545,7 +636,7 @@ def extend_trace(grid_size, propositions, static_cars, dependent_cars, component
     # place fixed movement cars
     dep_cars = [x for xs in components for x in xs.keys()]
     for c in set(fixed_movement_cars) - set(dep_cars):
-        moves[c] = dependent_cars[c]
+        moves[c] = fixed_movement_cars[c]
 
     # place independent cars
     for c in independent_cars:
