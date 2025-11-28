da058fe8864bba9a59ac830e57362ed5b4b9dd17
rosebohrer <rose.bohrer@aist.go.jp>
2025-11-28 18:00:43 +0900

    duration argument

--- checkers/baseline_version/BaselineEvaluatorTest.py
+++ checkers/baseline_version/BaselineEvaluatorTest.py
@@ -65,24 +65,24 @@ def same_name_test(test_index: int):
 # arbitrarily, sometimes staying still and other times moving forward. The second assumption says that SV follows POV1 as closely as possible without violating safety, i.e., we only
 # stay in place if POV is directly ahead. The conclusion says the two vehicles never collide.
 # Usefulness: We need a minimal, scalable "practical" example. This fills those requirements.
-def one_lane_follow_test(test_index:int, road_length: int):
+def one_lane_follow_test(test_index:int, duration: int, road_length: int):
     run_evaluator(test_index, [], ['z0', 'z1'], 
         ["@z0 !(Back 1)", #SV is initially at the start of the lane
          "G (@z1 ↓z2 ((! X 1) | X @z1  (z2 | Back z2)))", #POV always moves forward or stays put
          "G (@z0 ↓z2 ((! X 1) | X (@z0 ((!z1 & Back z2 ) | (z2 & Front z1) ))))"] , #SV Always moves forward if safe, stays put if POV immediately ahead
-         ["G(!(@z0 z1))"], (5,1), 3, False)
+         ["G(!(@z0 z1))"], (road_length,1), duration, False)
 
 # Test description: We model a 4-way intersection where one road has priority over the other, so that only one need to stop. We consider an N by N grid where the POV vehicle moves 
 # left-to-right without stopping. The SV moves back to front and stops if POV is on track to collide. z0 is SV, z1 is POV, z2 is temporary variable.
 # First assumption says POV moves in a straight line, second assumption says SV brakes (only) when needed, conclusion is collision freedom
 # Usefulness: The grid scales quadratically which allows us to generate large test cases easily. Crossing an intersection allows us to demonstrate a higher level of generality.
-def safe_intersection_priority(test_index: int, grid_size: int):
+def safe_intersection_priority(test_index: int, duration: int, grid_size: int):
     run_evaluator(test_index, [], ['z0', 'z1'], 
                   ["@z1 !(Left 1)", #z1 starts somewhere on the left border
                    "@z0 !(Back 1)", #z0 starts somewhere on the bottom border
                    "G (@z1 ↓z2 ((! X 1)| X @z1 (Left z2)))", #Moves left-to-right always
                    "G (@z0 ↓z2 ((! X 1)| X @z0 ((!z1 & Back z2) | (z2 & Front z1) )))"],  #Moves bottom-to-top except it stops to avoid other vehicle.
-                   ["G(!(@z0 z1))"], (grid_size, grid_size), 3, False)
+                   ["G(!(@z0 z1))"], (grid_size, grid_size), duration, False)
 
 # Test description: In this two-lane scenario, POV1 moves forward at uneven speed. Initially SV moves forward at even speed. 
 # If it is ever directly behind POV1, it swerves to left , then drives at high speed to overtake POV1, swerves right, then drives normally
@@ -104,7 +104,7 @@ def safe_intersection_priority(test_index: int, grid_size: int):
 #
 # Usefulness: Stress-tests the Until operator. "Non-trivial example". Helps demonstrate the value of optimizing one vehicle when the other clearly cannot be optimized 
 # (though we have other tests which demonstrate that same point)
-def safe_passing(test_index: int, road_length: int):
+def safe_passing(test_index: int, duration: int, road_length: int):
     # z0 initially moves forward
     first_forward = "(@z0 ↓z2 ((! X 1) | X @z0 (Back z2)))"
     # then swerves left to avoid z1
@@ -121,7 +121,7 @@ def safe_passing(test_index: int, road_length: int):
                    "@z0 !(Back 1)",
                    "G (@z1 ↓z2 ((! X 1) | X @z1  (z2 | Back z2)))", #z1 moves forward or stays in place
                    "({} U ({} & ((! X 1) | X ({} & ((! X 1) | X ({} U ({} & ((! X 1) | X G ({})))))))))".format(first_forward,dodge_left,fast_forward,fast_forward,dodge_right,last_forward)], 
-                   ["G(!(@z0 z1))"], (road_length, 2), 3, False)
+                   ["G(!(@z0 z1))"], (road_length, 2), duration, False)
     
 # Test description: In this test, a platoon of POV cars are all traveling at constant speed. The SV is trying to join the platoon. It can join the platoon by switching lanes
 # if the resulting position is both behind a car of the platoon and is safe.
@@ -129,7 +129,7 @@ def safe_passing(test_index: int, road_length: int):
 # which allows us to evaluate a different aspect of scalability compared to the other test cases.
 # Subtle note: We allow multiple cars in the platoon to have the same position as each other. We choose to see this as a feature instead of a bug, because it's equivalent
 # to testing all platoons of size *up to N* instead of size *exactly N*, e.g., if all cars in a 5-car platoon are in the same position, it functions as a 1-car platoon. 
-def join_platoon(test_index: int, platoon_size: int, road_length: int):
+def join_platoon(test_index: int, duration: int, platoon_size: int, road_length: int):
     pov_noms = ["z"+str(i+1) for i in range(platoon_size)]
     noms = ["z0"] + pov_noms  # and z is a temporary
     no_collide = "!({})".format(reduce((lambda x, acc: x + "|" + acc), pov_noms))
@@ -141,7 +141,7 @@ def join_platoon(test_index: int, platoon_size: int, road_length: int):
     pov_mov_assumps = [format("G(@z{0} ↓z ((! X 1) | X (@z{0} (Back z))))".format(str(i+1))) for i in range(platoon_size)]
     assumps = [sv_start_assump, sv_mov_assump] + pov_mov_assumps + pov_start_assumps
     postcond = "G(@z0 ({}))".format(no_collide)
-    run_evaluator(test_index, [], noms, assumps, [postcond], (road_length, 2), 3, False)
+    run_evaluator(test_index, [], noms, assumps, [postcond], (road_length, 2), duration, False)
 
 if __name__ == '__main__':
     # Test 1
@@ -149,10 +149,10 @@ if __name__ == '__main__':
     # Test 2
     same_name_test(2)
     #Test 3
-    one_lane_follow_test(3, 5)
+    one_lane_follow_test(3, 3, 5)
     #Test 4
-    safe_intersection_priority(4, 3)
+    safe_intersection_priority(4, 3, 3)
     #Test 5
-    safe_passing(5, 4)
+    safe_passing(5, 3, 4)
     #Test 6
-    join_platoon(6, 2, 2)
+    join_platoon(6, 3, 2, 2)
