6ba61f9c00946ccf123505b259c2f67ec5e0b1d4
rosebohrer <rose.bohrer@aist.go.jp>
2025-11-28 17:58:44 +0900

    add timeout logic

--- checkers/baseline_version/BaselineEvaluatorTest.py
+++ checkers/baseline_version/BaselineEvaluatorTest.py
@@ -1,15 +1,22 @@
+import multiprocessing
 from formula_types.HybridSpatioTemporalFormula import HybridSpatioTemporalFormula
 #from checkers.baseline_version.evaluator_baseline.SpatioTemporalEvaluator import evaluate
-from checkers.optimized_version.evaluator_optimized.SpatioTemporalEvaluator1 import evaluate
-#from checkers.optimized_version.evaluator_optimized.SpatioTemporalEvaluator2 import evaluate
+#from checkers.optimized_version.evaluator_optimized.SpatioTemporalEvaluator1 import evaluate 
+from checkers.optimized_version.evaluator_optimized.SpatioTemporalEvaluator2 import evaluate
 
-from timeit import default_timer as timer
+from timeit import default_timer as timer 
 # Rose apologizes to imperative programmers OTL
 from functools import reduce
 
 from parsers.HybridSpatioTemporalFormulaParser import HybridSpatioTemporalParser, tokenize
 
 
+def evaluate_handler(propositions, nominals, assumptions, conclusions, grid_size, trace_max_length, show_traces):
+   start: float = timer()
+   evaluate(propositions, nominals, assumptions, conclusions, grid_size, trace_max_length, show_traces)
+   end: float = timer()
+   print("|Time elapsed:", end - start, "\n")
+    
 def run_evaluator(run_id: int, propositions: list[str], nominals: list[str], assumptions: list[str], conclusions: list[str], grid_size: tuple[int, int], trace_max_length: int, show_traces: bool):
     """
     Returns for a given formula all (trace, points) tuples where the formula holds.
@@ -23,6 +30,7 @@ def run_evaluator(run_id: int, propositions: list[str], nominals: list[str], ass
     :param trace_max_length: the maximal length of traces to consider
     :param show_traces: whether the (trace, point) tuples should be displayed in the console
     """
+    TIMEOUT = 5    
 
     # conjunction of assumptions and conclusion
     input_formula_string: str = "&".join([*("(" + x + ")" for x in conclusions), *("(" + x + ")" for x in assumptions)])
@@ -39,11 +47,13 @@ def run_evaluator(run_id: int, propositions: list[str], nominals: list[str], ass
     print("|Maximal trace length:", trace_max_length)
 
     # evaluate formula and return traces and points where the formula holds
-    start: float = timer()
-    evaluate(propositions, nominals, assumptions, conclusions, grid_size, trace_max_length, show_traces)
-    end: float = timer()
-
-    print("|Time elapsed:", end - start, "\n")
+    p = multiprocessing.Process(target=evaluate_handler,args=(propositions, nominals, assumptions, conclusions, grid_size, trace_max_length, show_traces))
+    p.start()
+    p.join(TIMEOUT)
+    if p.is_alive():
+        print("=====================   TIMED OUT ===========================")
+        p.terminate()
+        p.join()
 
 def front_back_test(test_index: int):
     run_evaluator(test_index, [], ['z'], [], ["G(Left(Right(z)) <-> Right(Left(z)))"], (3,3), 3, False)
@@ -60,7 +70,7 @@ def one_lane_follow_test(test_index:int, road_length: int):
         ["@z0 !(Back 1)", #SV is initially at the start of the lane
          "G (@z1 ↓z2 ((! X 1) | X @z1  (z2 | Back z2)))", #POV always moves forward or stays put
          "G (@z0 ↓z2 ((! X 1) | X (@z0 ((!z1 & Back z2 ) | (z2 & Front z1) ))))"] , #SV Always moves forward if safe, stays put if POV immediately ahead
-         ["G(!(@z0 z1))"], (5,1), 3, True)
+         ["G(!(@z0 z1))"], (5,1), 3, False)
 
 # Test description: We model a 4-way intersection where one road has priority over the other, so that only one need to stop. We consider an N by N grid where the POV vehicle moves 
 # left-to-right without stopping. The SV moves back to front and stops if POV is on track to collide. z0 is SV, z1 is POV, z2 is temporary variable.
@@ -72,7 +82,7 @@ def safe_intersection_priority(test_index: int, grid_size: int):
                    "@z0 !(Back 1)", #z0 starts somewhere on the bottom border
                    "G (@z1 ↓z2 ((! X 1)| X @z1 (Left z2)))", #Moves left-to-right always
                    "G (@z0 ↓z2 ((! X 1)| X @z0 ((!z1 & Back z2) | (z2 & Front z1) )))"],  #Moves bottom-to-top except it stops to avoid other vehicle.
-                   ["G(!(@z0 z1))"], (grid_size, grid_size), 3, True)
+                   ["G(!(@z0 z1))"], (grid_size, grid_size), 3, False)
 
 # Test description: In this two-lane scenario, POV1 moves forward at uneven speed. Initially SV moves forward at even speed. 
 # If it is ever directly behind POV1, it swerves to left , then drives at high speed to overtake POV1, swerves right, then drives normally
@@ -111,7 +121,7 @@ def safe_passing(test_index: int, road_length: int):
                    "@z0 !(Back 1)",
                    "G (@z1 ↓z2 ((! X 1) | X @z1  (z2 | Back z2)))", #z1 moves forward or stays in place
                    "({} U ({} & ((! X 1) | X ({} & ((! X 1) | X ({} U ({} & ((! X 1) | X G ({})))))))))".format(first_forward,dodge_left,fast_forward,fast_forward,dodge_right,last_forward)], 
-                   ["G(!(@z0 z1))"], (road_length, 2), 3, True)
+                   ["G(!(@z0 z1))"], (road_length, 2), 3, False)
     
 # Test description: In this test, a platoon of POV cars are all traveling at constant speed. The SV is trying to join the platoon. It can join the platoon by switching lanes
 # if the resulting position is both behind a car of the platoon and is safe.
@@ -131,7 +141,7 @@ def join_platoon(test_index: int, platoon_size: int, road_length: int):
     pov_mov_assumps = [format("G(@z{0} ↓z ((! X 1) | X (@z{0} (Back z))))".format(str(i+1))) for i in range(platoon_size)]
     assumps = [sv_start_assump, sv_mov_assump] + pov_mov_assumps + pov_start_assumps
     postcond = "G(@z0 ({}))".format(no_collide)
-    run_evaluator(test_index, [], noms, assumps, [postcond], (road_length, 2), 3, True)
+    run_evaluator(test_index, [], noms, assumps, [postcond], (road_length, 2), 3, False)
 
 if __name__ == '__main__':
     # Test 1
