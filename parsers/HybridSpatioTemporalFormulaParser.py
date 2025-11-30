8b571204b774a18920710152a8fc192907a79cd9
radu.tulcan <radu.tulcan@tuwien.ac.at>
2025-11-30 17:36:40 +0100

    refactor, comment and create tests for the parser; add precedence relation of operators in the README

--- parsers/HybridSpatioTemporalFormulaParser.py
+++ parsers/HybridSpatioTemporalFormulaParser.py
@@ -1,42 +1,68 @@
 import re
-
+from typing import Union
 from formula_types.HybridSpatioTemporalFormula import HybridSpatioTemporalFormula
 from formula_types.HybridFormula import Nom, At, Bind
-from parsers.SpatioTemporalFormulaParser import SPATIOTEMPORAL_TOKEN_REGEX, SpatioTemporalParser
-from formula_types.ClassicalLogicFormula import Verum, Falsum, Prop, Not
+from formula_types.ClassicalLogicFormula import Verum, Falsum, Prop, Not, And, Iff, If, Or
 from formula_types.SpatialFormula import Front, Back, Left, Right
-from formula_types.TemporalFormula import Next, Eventually, Always
+from formula_types.TemporalFormula import Next, Eventually, Always, Until
 
+# classical logic syntax components
+TOP = "TOP"
+BOT = "BOT"
+PROP = "PROP"
+NOT = "NOT"
+AND = "AND"
+OR = "OR"
+IMPLIES = "IMPLIES"
+IFF = "IFF"
 
+# hybrid logic syntax components
 NOM = "NOM"
 AT = "AT"
 BIND = "BIND"
+
+# temporal logic syntax components
 NEXT = "NEXT"
 EVENTUALLY = "EVENTUALLY"
 ALWAYS = "ALWAYS"
 UNTIL = "UNTIL"
-SPACE = "SPACE"
-AND = "AND"
-TOP = "TOP"
-BOT = "BOT"
-NOT = "NOT"
+
+# spatial logic components
 FRONT = "FRONT"
 BACK = "BACK"
 LEFT = "LEFT"
 RIGHT = "RIGHT"
+
+# other syntax components
+SPACE = "SPACE"
 LPAREN = "LPAREN"
 RPAREN = "RPAREN"
-PROP = "PROP"
-
-# regex for hybrid syntax
-HYBRID_TOKEN_REGEX: str = r'''
-    | (?P<''' + NOM + '''>z[0-9_]*)
-    | (?P<''' + AT + '''>@z[0-9_]*)
-    | (?P<''' + BIND + '''>[↓:]z[0-9_]*)
-'''
 
 # regex for hybrid spatio-temporal formulas
-HYBRID_SPATIOTEMPORAL_TOKEN_REGEX: str = "".join([SPATIOTEMPORAL_TOKEN_REGEX, HYBRID_TOKEN_REGEX])
+HYBRID_SPATIOTEMPORAL_TOKEN_REGEX: str = r'''
+    (?P<''' + TOP + '''>⊤|1)
+  | (?P<''' + BOT + '''>⊥|0)
+  | (?P<''' + NOT + '''>¬|!)
+  | (?P<''' + AND + '''>∧|&)
+  | (?P<''' + OR + '''>v|\|)
+  | (?P<''' + IMPLIES + '''>→|->)
+  | (?P<''' + IFF + '''>↔|<->)
+  | (?P<''' + FRONT + '''>Front) 
+  | (?P<''' + BACK + '''>Back)
+  | (?P<''' + LEFT + '''>Left)
+  | (?P<''' + RIGHT + '''>Right)
+  | (?P<''' + LPAREN + '''>\()
+  | (?P<''' + RPAREN + '''>\))
+  | (?P<''' + PROP + '''>[a-y][a-y0-9_]*)
+  | (?P<''' + SPACE + '''>\s+)
+  | (?P<''' + NOM + '''>z[0-9_]*)
+  | (?P<''' + AT + '''>@z[0-9_]*)
+  | (?P<''' + BIND + '''>[↓:]z[0-9_]*)
+  | (?P<''' + NEXT + '''>X)
+  | (?P<''' + EVENTUALLY + '''>F)
+  | (?P<''' + ALWAYS + '''>G)
+  | (?P<''' + UNTIL + '''>U)
+  '''
 
 
 def tokenize(formula: str) -> list[tuple[str, str]]:
@@ -54,11 +80,117 @@ def tokenize(formula: str) -> list[tuple[str, str]]:
     return tokens
 
 
-class HybridSpatioTemporalParser(SpatioTemporalParser):
+class HybridSpatioTemporalParser:
+    """
+    Class for the hybrid spatio-temporal formula parser
+    """
+
+    def __init__(self, tokens: list[tuple[str, str]]):
+        self.tokens = tokens
+        self.pos = 0
+
+    def peek(self) -> Union[tuple[str, str], tuple[None, None]]:
+        """
+        Returns the next token in the array of tokens.
+
+        :return: the next token
+        """
+        return self.tokens[self.pos] if self.pos < len(self.tokens) else (None, None)
+
+    def consume(self, expected: str = None) -> tuple[str, str]:
+        """
+        Consumes the tokens one-by one and matches it against the expected string.
+
+        :param expected: string for the expected symbol
+        :return: the parsed token
+        """
+        tok = self.peek()
+        if expected and tok[0] != expected:
+            raise SyntaxError(f"Expected {expected}, got {tok}")
+        self.pos += 1
+        return tok
+
+    def parse(self) -> HybridSpatioTemporalFormula:
+        """
+        Parses a hybrid spatio-temporal formula.
+
+        :return: the parsed hybrid spatio-temporal formula
+        """
+        node: HybridSpatioTemporalFormula = self.parse_iff()
+        if self.pos != len(self.tokens):
+            raise SyntaxError("Unexpected tokens at end")
+        return node
+
+    def parse_iff(self) -> HybridSpatioTemporalFormula:
+        """
+        Parses a hybrid spatio-temporal formula with the uppermost operator a bi-implication.
+
+        :return: the parsed hybrid spatio-temporal formula
+        """
+        node: HybridSpatioTemporalFormula = self.parse_implies()
+        while self.peek()[0] == IFF:
+            self.consume()
+            right: HybridSpatioTemporalFormula = self.parse_implies()
+            node = Iff("↔", node, right)
+        return node
+
+    def parse_implies(self) -> HybridSpatioTemporalFormula:
+        """
+        Parses a hybrid spatio-temporal formula with the uppermost operator an implication.
+
+        :return: the parsed hybrid spatio-temporal formula
+        """
+        node: HybridSpatioTemporalFormula = self.parse_or()
+        while self.peek()[0] == IMPLIES:
+            self.consume()
+            right: HybridSpatioTemporalFormula = self.parse_or()
+            node = If("→", node, right)
+        return node
+
+    def parse_or(self) -> HybridSpatioTemporalFormula:
+        """
+        Parses a hybrid spatio-temporal formula with the uppermost operator a disjunction.
+
+        :return: the parsed hybrid spatio-temporal formula
+        """
+        node: HybridSpatioTemporalFormula = self.parse_and()
+        while self.peek()[0] == OR:
+            self.consume()
+            right: HybridSpatioTemporalFormula = self.parse_and()
+            node = Or("∨", node, right)
+        return node
+
+    def parse_and(self) -> HybridSpatioTemporalFormula:
+        """
+        Parses a hybrid spatio-temporal formula with the uppermost operator a conjunction.
+
+        :return: the parsed hybrid spatio-temporal formula
+        """
+        node: HybridSpatioTemporalFormula = self.parse_until()
+        while self.peek()[0] == AND:
+            self.consume()
+            right: HybridSpatioTemporalFormula = self.parse_until()
+            node = And("∧", node, right)
+        return node
+
+    def parse_until(self) -> HybridSpatioTemporalFormula:
+        """
+        Parses a hybrid spatio-temporal formula with the uppermost operator temporal until.
+
+        :return: the parsed hybrid spatio-temporal formula
+        """
+        node: HybridSpatioTemporalFormula = self.parse_unary()
+        while self.peek()[0] == UNTIL:
+            self.consume()
+            right: HybridSpatioTemporalFormula = self.parse_unary()
+            node = Until("U", node, right)
+        return node
+
     def parse_unary(self) -> HybridSpatioTemporalFormula:
         """
-        Parses unary hybrid spatio-temporal formulas.
-        :return: returns the parsed formula
+        Parses a hybrid spatio-temporal formula with the uppermost operator a unary operator.
+
+        :return: the parsed hybrid spatio-temporal formula
         """
         kind: str
         value: str
@@ -86,7 +218,7 @@ class HybridSpatioTemporalParser(SpatioTemporalParser):
             elif kind == AT:
                 return At(value[1:], value, operand)
             elif kind == BIND:
-                return Bind(value[1:], value.replace(":","↓"), operand)
+                return Bind(value[1:], value.replace(":", "↓"), operand)
         elif kind == LPAREN:
             self.consume(LPAREN)
             node: HybridSpatioTemporalFormula = self.parse_iff()
--- tests/TestHybridSpatioTemporalFormulaParser.py
+++ tests/TestHybridSpatioTemporalFormulaParser.py
@@ -33,7 +33,24 @@ class TestHybridSpatioTemporalFormulaParser(unittest.TestCase):
     def test_parse_nested_spatial_operators(self):
         self.assertTrue("(Left a) ↔ (@z1(Back(a | b)))" == str(HybridSpatioTemporalParser(tokenize("(Left(a)) <-> (@z1 Back(a | b))")).parse()))
 
+    def test_parse_simple_temporal_operators(self):
+        self.assertTrue("X(a ∧ b)" == str(HybridSpatioTemporalParser(tokenize("X(a & b)")).parse()))
+        self.assertTrue("F b" == str(HybridSpatioTemporalParser(tokenize("F(b)")).parse()))
+        self.assertTrue("G a" == str(HybridSpatioTemporalParser(tokenize("G  a")).parse()))
+        self.assertTrue("a U b" == str(HybridSpatioTemporalParser(tokenize("(a) U b")).parse()))
+
+    def test_parse_nested_temporal_operators(self):
+        self.assertTrue("(X(Left a)) ↔ (@z1(Back(a | b)))" == str(HybridSpatioTemporalParser(tokenize("(X(Left(a))) <-> (@z1 Back(a | b))")).parse()))
+        self.assertTrue("F(↓z2(c | d))" == str(HybridSpatioTemporalParser(tokenize("F(:z2(c | d))")).parse()))
+        self.assertTrue("(G(a U b)) | (@z1(F(X a)))" == str(HybridSpatioTemporalParser(tokenize("((G (a U b))) | (@z1(F(X a)))")).parse()))
+
     def test_parse_syntactically_wrong_formulas(self):
-        pass
+        self.assertRaises(SyntaxError, HybridSpatioTemporalParser(tokenize("b  a")).parse)
+        self.assertRaises(SyntaxError, HybridSpatioTemporalParser(tokenize("()")).parse)
+        self.assertRaises(SyntaxError, HybridSpatioTemporalParser(tokenize("b & | a")).parse)
+        self.assertRaises(SyntaxError, HybridSpatioTemporalParser(tokenize("F")).parse)
+        self.assertRaises(SyntaxError, HybridSpatioTemporalParser(tokenize("a (&) b)")).parse)
+
+
 if __name__ == '__main__':
     unittest.main()
